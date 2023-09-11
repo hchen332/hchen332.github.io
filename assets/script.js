cbe868488795b1c996d743252916484209a08672
hchen332 <hchen332@ucr.edu>
2023-09-11 02:00:24 -0700

    Some CSS changes, information about webpage

--- assets/script.js
+++ assets/script.js
@@ -28,7 +28,7 @@ const setAboutMe = () => {
     <p><b>Greetings!</b></p>
     <p>My name is <b>Coco</b>, I'm an aspiring <b>game developer</b> with a passion for video games.</p>
     <p>I have always loved games, and I have always engaged with my interests by <b>creating</b>. I consider myself an engineer and an artist who approaches every problem by seeking out <b>efficient</b> and <b>creative</b> solutions.</p>
-    <p>I am always taking on the challenge of learning new tools and skills. Check out some of my projects (including this webpage) by navigating with the each item slot!</p>
+    <p>I am always taking on the challenge of learning new tools and skills. Check out some of my projects (including this webpage) by navigating with each item slot!</p>
     `
 }
 setAboutMe();
@@ -46,6 +46,17 @@ item1.addEventListener("click", () => {
 
 item2.addEventListener("click", () => {
     contentBody.innerHTML = `
-    "<p>information body 2</p>"
+    <p>information body 2</p>
+    `
+});
+
+item6.addEventListener("click", () => {
+    contentBody.innerHTML = `
+    <h2>About This Webpage</h2>
+    <p>This webpage was built using Three.js and Vite, and hosted on Github Pages.</p>
+    <p>Three.js is the JavaScript library used to render the 3D model you see on the left. It is interactable, go ahead and rotate it!</p>
+    <p>The model, its rigs, and its animations were made by myself in Blender as part of a different project. In order to render the model on this webpage, I followed the steps on the Three.js documentation found on their site at <a href="https://threejs.org">threejs.org</a>.</p>
+    <p>The biggest challenge I encountered was properly building my project. Vite was not building the correct file paths for me.</p>
+    <p>After many, many hours of scouring the internet, I finally found my <a href="https://www.reddit.com/r/threejs/comments/z31v6w/gltf_files_not_found_in_production/">solution</a>. Storing my assets in an asset folder helped Vite identify the files it needed to bundle. Turns out, file structure is really important!</p>
     `
 });
