afeebda89f68559d19d1986887e3f7db02d25f75
hchen332 <hchen332@ucr.edu>
2023-09-11 18:41:23 -0700

    Added external links and some new icons

--- assets/script.js
+++ assets/script.js
@@ -24,11 +24,11 @@ const contentBody = document.getElementById("info-body");
 
 const setAboutMe = () => {
     contentBody.innerHTML = `
-    <h2>About Me</h2>
+    <h2>ABOUT ME</h2>
     <p><b>Greetings!</b></p>
     <p>My name is <b>Coco</b>, I'm an aspiring <b>game developer</b> with a passion for video games.</p>
     <p>I have always loved games, and I have always engaged with my interests by <b>creating</b>. I consider myself an engineer and an artist who approaches every problem by seeking out <b>efficient</b> and <b>creative</b> solutions.</p>
-    <p>I am always taking on the challenge of learning new tools and skills. Check out some of my projects (including this webpage) by navigating with each item slot!</p>
+    <p>I am always taking on the challenge of learning new tools and skills. Check out some of my projects (including this webpage) by navigating the item slots!</p>
     `
 }
 setAboutMe();
@@ -52,11 +52,10 @@ item2.addEventListener("click", () => {
 
 item6.addEventListener("click", () => {
     contentBody.innerHTML = `
-    <h2>About This Webpage</h2>
-    <p>This webpage was built using Three.js and Vite, and hosted on Github Pages.</p>
+    <h2>ABOUT THIS WEBPAGE</h2>
+    <p>This webpage was built from scratch using Three.js and Vite, and hosted on Github Pages.</p>
     <p>Three.js is the JavaScript library used to render the 3D model you see on the left. It is interactable, go ahead and rotate it!</p>
-    <p>The model, its rigs, and its animations were made by myself in Blender as part of a different project. In order to render the model on this webpage, I followed the steps on the Three.js documentation found on their site at <a href="https://threejs.org">threejs.org</a>.</p>
-    <p>The biggest challenge I encountered was properly building my project. Vite was not building the correct file paths for me.</p>
-    <p>After many, many hours of scouring the internet, I finally found my <a href="https://www.reddit.com/r/threejs/comments/z31v6w/gltf_files_not_found_in_production/">solution</a>. Storing my assets in an asset folder helped Vite identify the files it needed to bundle. Turns out, file structure is really important!</p>
+    <p>The model, its rigs, and its animations were made by myself as part of a different project. In order to render the model on this webpage, I followed the guide in the documentation for Three.js found on their site at <a href="https://threejs.org">threejs.org</a>.</p>
+    <p>The biggest challenge I encountered was properly building my project, Vite was not building the correct file paths for me. After many, many hours of scouring the internet, I finally found my <a href="https://www.reddit.com/r/threejs/comments/z31v6w/gltf_files_not_found_in_production/">solution</a>. Storing my assets in an asset folder helped Vite identify the files it needed to bundle. Turns out, file structure is really important!</p>
     `
 });
