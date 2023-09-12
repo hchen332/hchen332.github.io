0a237f752d499b4ba1b36aa10c4419e2755ba94c
hchen332 <hchen332@ucr.edu>
2023-09-11 23:25:08 -0700

    Iterate items, images scale with page

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 flexFont = function () {
     var div1 = document.getElementsByClassName("profile");
     var div2 = document.getElementsByClassName("information");
+    var div3 = document.getElementsByClassName("imgID");
     for (var i = 0; i < div1.length; i++) {
         var relFontsize1 = div1[i].offsetWidth * 0.03;
         div1[i].style.fontSize = relFontsize1 + 'px';
@@ -8,6 +9,10 @@ flexFont = function () {
     for (var j = 0; j < div2.length; j++) {
         var relFontsize2 = div2[j].offsetHeight * 0.03;
         div2[j].style.fontSize = relFontsize2 + 'px';
+    }
+    for (var k = 0; k < div3.length; k++) {
+        var relWidth3 = div3[k].parentElement.offsetHeight * 3;
+        div3[k].style.width = relWidth3 + 'px';
     }
 };
 
@@ -18,8 +23,6 @@ flexFont();
 
 //navigation buttons
 const item0 = document.getElementById("item0");
-const item1 = document.getElementById("item1");
-const item2 = document.getElementById("item2");
 const contentBody = document.getElementById("info-body");
 
 const setAboutMe = () => {
@@ -38,30 +41,17 @@ item0.addEventListener("click", () => {
     setAboutMe();
 });
 
-item1.addEventListener("click", () => {
-    fetch('./pages/item1.html')
-        .then(x => x.text())
-        .then(html => {
-            contentBody.innerHTML = html;
-        })
-        .catch(error => {
-            console.error('Error loading content: item1.html', error);
-        });
-});
+const items = document.getElementsByClassName("item");
 
-item2.addEventListener("click", () => {
-    contentBody.innerHTML = `
-    <p>information body 2</p>
-    `
-});
-
-item6.addEventListener("click", () => {
-    fetch('./pages/item6.html')
-        .then(x => x.text())
-        .then(html => {
-            contentBody.innerHTML = html;
-        })
-        .catch(error => {
-            console.error('Error loading content: item6.html', error);
-        });
-});
+for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", () => {
+        fetch(`./pages/item${i + 1}.html`)
+            .then(x => x.text())
+            .then(html => {
+                contentBody.innerHTML = html;
+            })
+            .catch(error => {
+                console.error(`Error loading content: item${i + 1}.html`, error);
+            });
+    });
+}
